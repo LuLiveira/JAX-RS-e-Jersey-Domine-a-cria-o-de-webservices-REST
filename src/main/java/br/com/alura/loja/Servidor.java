e557b61d2bb3e4725dc697a3fc1ad65831cdefcf
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-12-29 13:45:28 -0300

    Testes end-to-end com JAX-RS e Grizzly

--- src/main/java/br/com/alura/loja/Servidor.java
+++ src/main/java/br/com/alura/loja/Servidor.java
@@ -11,15 +11,20 @@ public class Servidor {
 
 	public static void main(String[] args) throws IOException {
 
-		URI uri = URI.create("http://localhost:8080/");
-		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		HttpServer server = inicializaServer();
 
 		System.out.println("Server running...");
 
 		System.in.read();
 
 		server.stop();
+	}
+
+	static HttpServer inicializaServer() {
+		URI uri = URI.create("http://localhost:8080/");
+		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
 	}
 
 }
--- src/test/java/br/com/alura/loja/ClientTest.java
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -4,18 +4,37 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
 import junit.framework.Assert;
 
 public class ClientTest {
+
+	private HttpServer server;
+
+	@Before
+	public void startServer() {
+		this.server = Servidor.inicializaServer();
+	}
+
+	@After
+	public void stopServer() {
+		this.server.stop();
+	}
 	
 	@Test
-	public void testaQueAConexaoComOServidorFunciona() {
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String response = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		Assert.assertTrue(response.contains("<rua>"));		
+		WebTarget target = client.target("http://localhost:8080/");
+		String response = target.path("carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(response);
+		Assert.assertTrue(carrinho.getRua().equals("Rua Vergueiro 3185, 8 andar"));
 	}
 
 }
